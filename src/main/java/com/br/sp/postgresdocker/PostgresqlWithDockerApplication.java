fa17946449b576f97086724e1d6c861921f34312
Bruno Mathidios EDS <brunomathidios@yahoo.com.br>
2018-06-30 15:47:56 -0300

    commit teste

---
+++ src/main/java/com/br/sp/postgresdocker/PostgresqlWithDockerApplication.java
@@ -0,0 +1,12 @@
+package com.br.sp.postgresdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresqlWithDockerApplication.class, args);
+	}
+}
